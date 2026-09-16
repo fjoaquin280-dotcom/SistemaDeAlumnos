af59eb684c48c7e4984a77e5d43c729c1778d71a
fjoaquin280-dotcom <fjoaquin280@gmail.com>
2026-09-16 20:11:50 -0300

    Etapa 2 — Constructor

--- Program.cs
+++ Program.cs
@@ -1,18 +1,10 @@
 ﻿using SistemaDeAlumnos;
 
-Alumno alumno1 = new Alumno();
+Alumno alumno1 = new Alumno("Joaquin", 123, 8.5, 7.0);
 
-alumno1.Nombre = "Joaquin";
-alumno1.Legajo = 123;
-alumno1.Nota1 = 8.5;
-alumno1.Nota2 = 7.0;
+Alumno alumno2 = new Alumno("Sofia", 456, 9.0, 10.0);
 
-Alumno alumno2 = new Alumno();
-alumno2.Nombre = "Sofia";
-alumno2.Legajo = 456;
-alumno2.Nota1 = 9.0;
-alumno2.Nota2 = 10.0;
-
+Alumno alumno3 = new Alumno();
 // Mostrar nombre y legajo
 Console.WriteLine($"{alumno1.Nombre} - {alumno1.Legajo}");
 Console.WriteLine($"{alumno2.Nombre} - {alumno2.Legajo}");
