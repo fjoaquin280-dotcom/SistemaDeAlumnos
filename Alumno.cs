a0d7eed17e0455715aaa9344ca4bfc5fd79f03ad
fjoaquin280-dotcom <fjoaquin280@gmail.com>
2026-09-16 19:54:11 -0300

    Etapa 1 — La primera clase

---
+++ Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaDeAlumnos
+{
+    public class Alumno
+    {
+        public string Nombre { get; set; }
+        public int Legajo { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SistemaDeAlumnos;
+
+Alumno alumno1 = new Alumno();
+
+alumno1.Nombre = "Joaquin";
+alumno1.Legajo = 123;
+alumno1.Nota1 = 8.5;
+alumno1.Nota2 = 7.0;
+
+Alumno alumno2 = new Alumno();
+alumno2.Nombre = "Sofia";
+alumno2.Legajo = 456;
+alumno2.Nota1 = 9.0;
+alumno2.Nota2 = 10.0;
+
+// Mostrar nombre y legajo
+Console.WriteLine($"{alumno1.Nombre} - {alumno1.Legajo}");
+Console.WriteLine($"{alumno2.Nombre} - {alumno2.Legajo}");
+
+alumno1.Nombre = "Pedro";
+
+Console.WriteLine("\nDespués del cambio:");
+Console.WriteLine($"{alumno1.Nombre} - {alumno1.Legajo}");
+Console.WriteLine($"{alumno2.Nombre} - {alumno2.Legajo}");
